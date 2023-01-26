252d4d62775b8e16447736dc0fea6be76c0a9fc1
Audrey Fuller <alf9310@rit.edu>
2023-01-26 16:51:29 -0500

    Finished activity

--- week02/IO_activity.py
+++ week02/IO_activity.py
@@ -1,17 +1,19 @@
 """
-What goes here???
+Audrey Fuller (alf9310)
+Practice I/O operations
 """
 
 #We're going to practice reading input, storing it, and printing it
 #Prompt the user to read everyone's name for as many people there are
-#Store the names in seperate variables
+#Store  names in a variable
+name = input("What is your name? ")
 
 #What type are these variables stored as? Which characters can this type hold?
 
 #What are the two ways to run this program?
 
-#Print the names to terminal, along with the description: "Names:"
-
+#Print the names to terminal, along with the description: "Name:"
+print("Name: " + "\"" + name + "\"")
 #How do you print " " or ' '?
 
 #Time to push to git!
