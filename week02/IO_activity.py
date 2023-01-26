111450c31b7fe9267cb80398b8883f26519cab5b
Audrey Fuller <alf9310@rit.edu>
2023-01-26 15:22:02 -0500

    activity prepped

--- week02/IO_activity.py
+++ week02/IO_activity.py
@@ -1,1 +1,17 @@
-print("hello world!")
+"""
+What goes here???
+"""
+
+#We're going to practice reading input, storing it, and printing it
+#Prompt the user to read everyone's name for as many people there are
+#Store the names in seperate variables
+
+#What type are these variables stored as? Which characters can this type hold?
+
+#What are the two ways to run this program?
+
+#Print the names to terminal, along with the description: "Names:"
+
+#How do you print " " or ' '?
+
+#Time to push to git!
