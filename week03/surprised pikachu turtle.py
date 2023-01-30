53354e7fb90e5a997c4d02f80bbe86da95983d54
Audrey Fuller <alf9310@rit.edu>
2023-01-30 09:07:59 -0500

    Week three activity A added

---
+++ week03/surprised pikachu turtle.py
@@ -0,0 +1,31 @@
+"""
+Week 3 Session A: Turtle activity to build farmiliarity with turtle by creating 
+a suprised pikachu drawing.
+Concepts: Scope, Functions, Parameters, Incremental Developement, Turtle
+@author: Audrey Fuller
+Adapted from Zoe Bingham activity
+"""
+
+# Import the library for turtle
+
+# Initialize a few global variables for a default color, location, turtle size, and pen size
+
+# Initialize static global variables for the colors of pikachu
+
+# Make a function to draw the base of a pikachu (Use given function below)
+# def draw_arc(degrees):
+#    for increment in range(180):
+#        t.forward(1)
+#        t.right(1)
+
+# Make a function to draw the pikachu eyes
+
+# Make a function to draw the pikachu nose
+
+# Make a function to draw the pikachu cheek blush
+
+# Make a function to draw the pikachu mouth
+
+# Make a function that puts all of the elements of pikachu together with parameters for the x and y coords and size
+
+# Call our pikachu function multiple times in our main method 
