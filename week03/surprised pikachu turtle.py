7f0d85f94711df95e7d622c6aef4764f64c4f219
Audrey Fuller <alf9310@rit.edu>
2023-01-30 13:17:36 -0500

    Added in-class tree activity review

--- week03/surprised pikachu turtle.py
+++ week03/surprised pikachu turtle.py
@@ -7,6 +7,65 @@ Concepts: Scope, Functions, Parameters, Incremental Developement, Turtle
 Adapted from Zoe Bingham activity
 """
 
+#In-class tree activity review
+import turtle as t
+
+def reset_turtle():
+    t.tracer(False)
+    t.penup()
+    t.goto(0,0)
+    t.speed(0)
+    t.pencolor("black")
+
+def triangle(x, y, size, pen, fill):
+    t.goto(x, y)
+    t.pencolor(pen)
+    t.fillcolor(fill)
+    t.begin_fill()
+    t.pendown()
+    t.forward(size)
+    t.left(120)
+    t.forward(size)
+    t.left(120)
+    t.forward(size)
+    t.left(120)
+    t.end_fill()
+    reset_turtle()
+
+def rectangle(x,y,width,height,pen,fill):
+    t.goto(x, y)
+    t.pencolor(pen)
+    t.fillcolor(fill)
+    t.begin_fill()
+    t.pendown()
+    t.forward(width)
+    t.left(90)
+    t.forward(height)
+    t.left(90)
+    t.forward(width)
+    t.left(90)
+    t.forward(height)
+    t.left(90)
+    t.end_fill()
+    reset_turtle()
+
+def tree(x,y,size):
+    t.penup()
+    t.goto(x, y)
+    rectangle(x,y,size/2,size*2,"brown","brown")
+    triangle(x - size, y + size*2, size*2.5, "green", "green")
+    triangle(x - size, y + size*3, size*2.5, "green", "green")
+    reset_turtle()
+
+def main():
+    tree(-100,-300,100)
+    input()
+
+main()
+
+
+#Surprised Pikachu Outline 
+
 # Import the library for turtle
 
 # Initialize a few global variables for a default color, location, turtle size, and pen size
@@ -14,10 +73,10 @@ Adapted from Zoe Bingham activity
 # Initialize static global variables for the colors of pikachu
 
 # Make a function to draw the base of a pikachu (Use given function below)
-def draw_arc(degrees):
-    for increment in range(180):
-        t.forward(1)
-        t.right(1)
+#def draw_arc(degrees):
+#    for increment in range(180):
+#        t.forward(1)
+#        t.right(1)
 
 # Make a function to draw the pikachu eyes
 
