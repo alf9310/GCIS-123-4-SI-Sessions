14fd4ad7378b8db576744bf263efe70d15235c54
Audrey Fuller <alf9310@rit.edu>
2023-02-06 08:49:23 -0500

    Unit testing added

---
+++ week04/unit_testing.py
@@ -0,0 +1,9 @@
+"""
+Session 4A: Introducing Return statements, unit testing, running pytest
+            code paths(nested functions), refactoring, conditionals
+@author: Audrey Fuller (alf9310)
+         (some functions adapted from Zoe Bingham)
+"""
+
+# What should we name the test file? What do we import?
+
