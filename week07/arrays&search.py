93bd99301a0910274fc2c2ccf747edf98c320fe9
Audrey Fuller <alf9310@rit.edu>
2023-02-27 09:31:52 -0500

    added week 7 activity

---
+++ week07/arrays&search.py
@@ -0,0 +1,62 @@
+"""
+Session 7A: Arrays, Random & Linear Search Coding Activity.
+@author: Audrey Fuller
+"""
+
+import arrays
+import time
+
+'''
+Make an array with even numbers from start to end
+Parameters:
+    :param int start: beginning of the array
+    :param int end: ending of the array
+'''
+def make_even_array(start, end):
+    return
+
+
+'''
+Find the first duplicate element in a given array of integers. 
+Return -1 If there are no such elements.
+Parameters:
+    :param int array: the array of ints
+'''
+def find_first_duplicate(array):
+    pass
+
+'''
+Replaces characters in an array.
+Parameters:
+    :param int array: the array of ints
+    :param int new: new character
+    :param int end: old character to replace
+'''
+def replace_array(array, new, old):
+    pass
+
+'''
+Fill an array with random numbers using the passed in seed
+Parameters:
+    :param int size: size of the array
+    :param int seed: seed for random
+'''
+def random_array(size, seed):
+    return
+
+'''
+Search an array for the specified value using the linear search in an array
+Parameters:
+:param int array: array to be searched
+:param int n: the value to be found
+'''
+def linear_search(array, n):
+    return
+
+
+def main():
+    name = arrays.Array(5, 1)
+    print(name)
+
+if __name__ == '__main__':
+    main()
