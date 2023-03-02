ec6adbecf71e5657e55107032b529b0041392682
Audrey Fuller <alf9310@rit.edu>
2023-03-02 18:30:57 -0500

    SI session

--- week07/recursion.py
+++ week07/recursion.py
@@ -14,9 +14,13 @@ Parameters:
 '''
 def exponent(base, exp):
     #base case?
-
+    if exp == 0:
+        print('Base Case')
+        return 1
     #recursive case?
-    return
+    else: 
+        print("base " + str(base) + "   exponent: " + str(exp))
+        return base * exponent(base, exp - 1)
 
 '''
 Recursive function to reverse the words in a string
@@ -25,17 +29,20 @@ Parameters:
     :param int index:
 '''
 def reverse_string(string, index = 0):
-    #base case?
-
-    #recursive case?
-    return
+   reverse_str = ""
+   words = string.split()
+   if len(words) == index: # Base case
+       return ""
+   else: # Recursive case
+       reverse_str += reverse_string(string, index + 1) + words[index] + " "
+   return reverse_str
 
 
 def main():
     #array = [1, 2, 4, 6, 6, 8, 10, 11, 11]
-    #print(exponent(10, 2))
-    string = "hello world!"
-    print(string)
+    print(exponent(6, 10))
+    #string = "hello world!"
+    #print(string)
     #print(reverse_string(string))
 
 if __name__ == '__main__':
