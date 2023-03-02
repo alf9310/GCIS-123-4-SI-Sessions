81d2bd991eb1b0e42ff8a03931a58cebaa12a3ee
Audrey Fuller <alf9310@rit.edu>
2023-03-02 15:53:51 -0500

    Recursive created

---
+++ week07/recursion.py
@@ -0,0 +1,42 @@
+"""
+Session 7B: Recursion
+@author: Audrey Fuller
+"""
+
+import arrays
+import time
+
+'''
+Find the base ^ exponent
+Parameters:
+    :param int base:
+    :param int exponent:
+'''
+def exponent(base, exp):
+    #base case?
+
+    #recursive case?
+    return
+
+'''
+Recursive function to reverse the words in a string
+Parameters:
+    :param String string:
+    :param int index:
+'''
+def reverse_string(string, index = 0):
+    #base case?
+
+    #recursive case?
+    return
+
+
+def main():
+    #array = [1, 2, 4, 6, 6, 8, 10, 11, 11]
+    #print(exponent(10, 2))
+    string = "hello world!"
+    print(string)
+    #print(reverse_string(string))
+
+if __name__ == '__main__':
+    main()
