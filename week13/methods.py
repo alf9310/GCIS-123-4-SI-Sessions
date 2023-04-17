1e13cd3216f7bb6143b408d8205c39500033da68
Audrey Fuller <alf9310@rit.edu>
2023-04-17 11:39:50 -0400

    methods created

---
+++ week13/methods.py
@@ -0,0 +1,54 @@
+"""
+Session 13A: Class Methods
+@author: Audrey Fuller
+"""
+
+class Dog:
+    # TODO: Only allow the dog to have the following attributes: name, breed, age
+
+    # TODO: Make a constructor for the dog
+
+    # TODO: Overwrite the default string representation method to return an appropriate string representation of the dog
+
+    # TODO: Overwrite the string method to return a string with the following format: 
+    # " Name:       Bailey
+    #   Type:       Lab; [Brown, Tan, Black]; 10 years
+    #   Age:        5 years"
+
+    # TODO: Make a method that will allow the dogs to be sorted by alphabetical order by name
+
+    # TODO: Make this hashable.
+
+    # TODO: CHALLENGE: Make a method that will allow the dogs to be sorted by age.(If you do this, you will need to comment out your other sortable method).
+
+    pass
+
+class DogBreed:
+    # TODO: Only allow the Dog Breed to have the following attributes: breed_name, coat_colors, lifespan
+
+    # TODO: Make a constructor for Dog Breed
+
+    # TODO: Overwrite the default string representation method to return an appropriate string representation of the dog
+
+    # TODO: Overwrite the string method to return a string with the following format: 
+    # "Lab; [Brown, Tan, Black]; 10 years"
+
+    # TODO: Make a method that will allow the dog breeds to be sorted by alphabetical order by name
+
+    # TODO: Make this hashable.
+
+    pass
+
+# TODO: Make a function that returns a dictionary of bugs with bug_type as a key and bug as the value. (ex: "Insect": {bug1, bug2, bug3})
+
+
+def main():
+    # TODO: Print all dogs in sorted order
+
+    # TODO: Print the value for the "Lab" section in your dictionary
+
+    # TODO: CHALLENGE: Prompt the user for input to check if the dog exists. If it doesn't, allow them to create the dog.
+    pass
+
+if __name__ == "__main__":
+    main()
